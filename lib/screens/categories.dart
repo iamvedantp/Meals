ad1273fcdebc1f706e36d912c1151f57613eaf7a
iamvedantp <vedantp183@gmail.com>
2024-02-05 21:17:47 +0530

    meal added

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,30 +1,50 @@
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen ({super.key});
-  
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context,Category category) {
+  final filteredMeals = dummyMeals.where((Meal) => Meal.categories.contains(category.id)).toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar (
-      title: Text('Pick your category'),
-    )
-    ,body: GridView(
-      gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
       ),
-      children: const[
-        Text('1',style: TextStyle(color: Colors.white),),
-        Text('2',style: TextStyle(color: Colors.white),),
-        Text('3',style: TextStyle(color: Colors.white),),
-        Text('4',style: TextStyle(color: Colors.white),),
-        Text('5',style: TextStyle(color: Colors.white),),
-        Text('6',style: TextStyle(color: Colors.white),),
-      ],
-    ),
-  );
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+        ],
+      ),
+    );
   }
 }
