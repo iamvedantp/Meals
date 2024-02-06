c7582dfbc55a9c30d7ae04a3f23c55e0a272de97
iamvedantp <vedantp183@gmail.com>
2024-02-06 19:58:12 +0530

    improved

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Category {
-  const Category ({
-    required this.id, required this.title,this.color = Colors.orange,
+  const Category({
+    required this.id,
+    required this.title,
+    this.color = Colors.orange,
   });
 
   final String id;
