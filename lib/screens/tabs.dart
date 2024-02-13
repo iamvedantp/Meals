d004800583e21731f31311f94e3ad85b1a7e137f
iamvedantp <vedantp183@gmail.com>
2024-02-13 14:57:31 +0530

    Updated

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -3,7 +3,6 @@ import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
-import 'package:meals/providers/meals_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/providers/favorites_provider.dart';
 import 'package:meals/providers/filters_provider.dart';
@@ -48,23 +47,8 @@ await Navigator.of(context).push<Map<Filter,bool>>(
 
   @override
   Widget build(BuildContext context) {
-  final meals =ref.watch(mealsProvider);  
-  final activeFilters = ref.watch(filtersProvider);
-  final avaliableMeals = meals.where((meal) {
-    if (activeFilters[Filter.glutenfree]! && !meal.isGlutenFree) {
-      return false;
-    }
-    if (activeFilters[Filter.lactosefree]! && !meal.isLactoseFree) {
-      return false;
-    }
-    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-      return false;
-    }
-    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-      return false;
-    }
-     return true;
-  }).toList();
+  
+  final avaliableMeals = ref.watch(filteredMealsIsProvider);
 
 
     Widget activePage = CategoriesScreen(
