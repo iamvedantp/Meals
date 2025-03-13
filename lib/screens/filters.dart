18d06fe304fae99f40b0770417ae0c474917fd75
iamvedantp <vedantp183@gmail.com>
2025-03-13 19:34:35 +0530

    file renamed

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meals/providers/filteredMealsProvider.dart';
+import 'package:meals/providers/filteredmealsprovider.dart';
 
 /// A reusable widget for displaying a filter switch.
 class FilterSwitchTile extends StatelessWidget {
